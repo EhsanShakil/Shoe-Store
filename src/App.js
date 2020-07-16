f4bc00e65ac470ccd571a1d358d3d4d11e1eaab3
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-07-16 20:29:36 +0500

    Updating

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
+import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router";
 import Home from "./Components/Screens/Home";
 import Store from "./Components/Screens/Store";
@@ -14,7 +15,7 @@ const NotFound = () => {
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
@@ -24,7 +25,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
